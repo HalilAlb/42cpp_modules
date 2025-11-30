31d5922bb20944d204e408fe0e1c78688638520e
HalilAlb <halil_3425@hotmail.com>
2025-11-30 19:34:49 +0300

    cpp01 ex06'da ex05 ile aynı fakat switch case işle yapılacak o yüzden kodlar aynı hemen hemen

--- cpp01/ex06/Harl.hpp
+++ cpp01/ex06/Harl.hpp
@@ -6,7 +6,27 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 18:23:53 by malbayra          #+#    #+#             */
-/*   Updated: 2025/11/30 18:23:54 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/11/30 19:04:26 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _Harl_
+# define _Harl_
+
+#include <iostream>
+
+class Harl
+{
+  public:
+	Harl();
+	~Harl();
+	void complain(std::string level);
+
+  private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+};
+
+#endif
