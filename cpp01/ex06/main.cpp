035e34c8c1d8779e9dd33c2639cbeb4e83c0597e
Halil Albayrak <109541183+HalilAlb@users.noreply.github.com>
2025-12-01 03:49:43 +0300

    laptobumdan pushlayamadım hata verdi githubtan pushluyorum :)
    Added a function to get the index of the log level and modified the main function to handle command line arguments for logging levels.

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -10,3 +10,48 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int	getLevelIndex(std::string level)
+{
+	const char *levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (level == levels[i])
+			return (i);
+	}
+	return (-1);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./harlFilter <level>" << std::endl;
+		return (1);
+	}
+
+	Harl harl;
+	int levelIndex = getLevelIndex(argv[1]);
+
+	switch (levelIndex)
+	{
+		case 0:
+			harl.complain("DEBUG");
+			std::cout << std::endl;
+		case 1:
+			harl.complain("INFO");
+			std::cout << std::endl;
+		case 2:
+			harl.complain("WARNING");
+			std::cout << std::endl;
+		case 3:
+			harl.complain("ERROR");
+			std::cout << std::endl;
+			break ;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
+	return (0);
+}
