85ef9a68ddd957428f451d2c9ed9da68d00860a8
HalilAlb <halil_3425@hotmail.com>
2025-12-05 18:52:05 +0300

    cp01 ex06 arguman kontrolu yanlıslıkla değişilmiş düzeltildi

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 18:23:36 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/05 17:12:32 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/05 18:51:38 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	getLevelIndex(std::string level)
 
 int	main(int argc, char **argv)
 {
-	if (argc != 1)
+	if (argc != 2)
 	{
 		std::cerr << "Usage: ./harlFilter <level>" << std::endl;
 		return (1);
