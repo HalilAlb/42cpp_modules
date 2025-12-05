e317978f1a222d6e1f7610f82270d3204a50f311
HalilAlb <halil_3425@hotmail.com>
2025-12-05 18:58:15 +0300

    cp02 ex00 main subjecten alınıp eklendi

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -6,7 +6,21 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 19:34:16 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/02 19:34:17 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/05 18:57:34 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+#include <iostream>
+
+int	main(void)
+{
+	Fixed a;
+	Fixed b(a);
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return (0);
+}
