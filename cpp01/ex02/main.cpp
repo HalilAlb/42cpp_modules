80d517044857e659a99c2c447aa633e3de3e405c
HalilAlb <halil_3425@hotmail.com>
2025-12-08 15:37:05 +0300

    cpp01 ex02 value eklendi

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,24 +6,25 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 23:36:53 by malbayra          #+#    #+#             */
-/*   Updated: 2025/11/13 23:41:39 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/08 15:36:01 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main( void )
+int	main(void)
 {
-	std::string a =  "HI THIS IS BRAIN";
-	std::string* stringPTR = &a;
-	std::string& stringREF = a;
-	
+	std::string a = "HI THIS IS BRAIN";
+	std::string *stringPTR = &a;
+	std::string &stringREF = a;
+
 	std::cout << "Adresse of a (value): " << &a << std::endl;
 	std::cout << "Adresse of a (pointer): " << stringPTR << std::endl;
 	std::cout << "Adresse of a (REF): " << &stringREF << std::endl;
-
-	std::cout << std::endl << "Value of a (pointer): " << *stringPTR << std::endl;
-	std::cout << "Value of a (ref): " << stringREF << std::endl;
 	
+	std::cout << std::endl << "Value of a (value): " << a << std::endl;
+	std::cout << "Value of a (pointer): " << *stringPTR << std::endl;
+	std::cout << "Value of a (ref): " << stringREF << std::endl;
+
 	return (0);
 }
--- cpp01/ex03/main.cpp
+++ cpp01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 17:06:50 by malbayra          #+#    #+#             */
-/*   Updated: 2025/11/27 19:48:02 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/08 15:36:30 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 int main()
 {
 {
-Weapon club = Weapon("crude spiked club");
+Weapon club = Weapon("crude spiked club"); // weapon için constroctur oluşturulacak
 HumanA bob("Bob", club);
 bob.attack();
 club.setType("some other type of club");
