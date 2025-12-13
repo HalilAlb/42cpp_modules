6577e174a45b9121a2effd919bcd168ee57ad16f
HalilAlb <halil_3425@hotmail.com>
2025-12-13 16:48:17 +0300

    cpp02 ex01 ex00 dan koplayandı fakat çalışmıyor düzenlemeler yapılacak istenilenlere göre hpp düzenlenecek ve cpp de eklenecek

--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,29 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 19:12:02 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/08 19:12:03 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/11 17:41:08 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef _Fixed_
+# define _Fixed_
+
+#include <iostream>
+
+class Fixed
+{
+    public:
+	Fixed();                            
+	Fixed(const Fixed &other);      
+	Fixed &operator=(const Fixed &other); 
+	~Fixed();                            
+
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+
+  private:
+	int _rawBits;
+	static const int _fractionalBits = 8;
+};
+
+#endif
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,26 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 19:11:59 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/08 19:12:00 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/11 17:40:27 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+int	main(void)
+{
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+	a = Fixed(1234.4321f);
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	return (0);
+}
