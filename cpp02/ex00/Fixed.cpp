95b6bda23023ba082bc629691c276027d04eb0b4
HalilAlb <halil_3425@hotmail.com>
2025-12-14 16:49:11 +0300

    cpp02 ex00 farklı şekilde düzenlemeler yapıldı best practise düzeyinde yapmaya çalısıyorum şuan bir sorun yok

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,38 +6,44 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 19:34:32 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/08 19:01:07 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/14 16:47:02 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed() : _rawBits(0) {
-    std::cout << "Default constructor called" << std::endl;
+Fixed::Fixed() : _rawBits(0)
+{
+	std::cout << "Default constructor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed &other) {
-    std::cout << "Copy constructor called" << std::endl;
-    this->_rawBits = other.getRawBits();
+Fixed::Fixed(const Fixed &other) : _rawBits(other.getRawBits())
+{
+	std::cout << "Copy constructor called" << std::endl;
 }
 
-Fixed &Fixed::operator=(const Fixed &other) {
-    std::cout << "Copy assignment operator called" << std::endl;
-    if (this != &other) {
-        this->_rawBits = other.getRawBits();
-    }
-    return *this;
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		_rawBits = other.getRawBits();
+	}
+	return *this;
 }
 
-Fixed::~Fixed() {
-    std::cout << "Destructor called" << std::endl;
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
 }
 
-int Fixed::getRawBits(void) const {
-    std::cout << "getRawBits member function called" << std::endl;
-    return this->_rawBits;
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return _rawBits;
 }
 
-void Fixed::setRawBits(int const raw) {
-    this->_rawBits = raw;
+void Fixed::setRawBits(int const raw)
+{
+	_rawBits = raw;
 }
--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -6,27 +6,29 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/02 19:34:42 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/08 18:58:10 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/14 16:48:02 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _Fixed_
-# define _Fixed_
+#ifndef FIXED_HPP
+# define FIXED_HPP
 
-#include <iostream>
+# include <iostream>
 
 class Fixed
 {
-    public:
-	Fixed();                            
-	Fixed(const Fixed &other);      
-	Fixed &operator=(const Fixed &other); 
-	~Fixed();                            
+public:
+
+	Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &other);
+	~Fixed();
+
 
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 
-  private:
+private:
 	int _rawBits;
 	static const int _fractionalBits = 8;
 };
