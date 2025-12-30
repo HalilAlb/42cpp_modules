463c9d122c6f74085d21a93676e818f3f89491ae
HalilAlb <halil_3425@hotmail.com>
2025-12-30 04:05:45 +0300

    fragtrap yazıldı main işlemeli olarak dolduruldu suan bir sorun göremedim varsa kampüste kontrol edilecek

--- cpp03/ex02/FragTrap.cpp
+++ cpp03/ex02/FragTrap.cpp
@@ -6,9 +6,49 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 20:00:00 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/28 21:00:52 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/28 21:04:13 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
+FragTrap::FragTrap() : ClapTrap()
+{
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap default constructor called!" << std::endl;
+}
+
+FragTrap::FragTrap(const std::string& name) : ClapTrap(name)
+{
+    _hitPoints = 100;
+    _energyPoints = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap " << _name << " has been constructed!" << std::endl;
+}
+
+FragTrap::FragTrap(const FragTrap& other) : ClapTrap(other)
+{
+    std::cout << "FragTrap " << _name << " has been copy constructed!" << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& other)
+{
+    std::cout << "FragTrap " << _name << " assignment operator called!" << std::endl;
+    if (this != &other)
+    {
+        ClapTrap::operator=(other);
+    }
+    return *this;
+}
+
+FragTrap::~FragTrap()
+{
+    std::cout << "FragTrap " << _name << " has been destroyed!" << std::endl;
+}
+
+void FragTrap::highFivesGuys(void)
+{
+    std::cout << "FragTrap " << _name << " is requesting a positive high five! ✋" << std::endl;
+}
--- cpp03/ex02/main.cpp
+++ cpp03/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: malbayra <malbayra@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 20:00:00 by malbayra          #+#    #+#             */
-/*   Updated: 2025/12/28 21:01:03 by malbayra         ###   ########.fr       */
+/*   Updated: 2025/12/28 21:04:13 by malbayra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,166 @@
 
 int main()
 {
-   
+    std::cout << "=== Test 1: Construction/Destruction Chaining ===" << std::endl;
+    {
+        std::cout << "\n--- Creating FragTrap ---" << std::endl;
+        FragTrap frag("Demolisher");
+        std::cout << "\n--- Destroying FragTrap ---" << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 2: FragTrap High Fives ===" << std::endl;
+    {
+        FragTrap frag("Friendly");
+        frag.highFivesGuys();
+        frag.attack("Target");
+        frag.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 3: FragTrap Attack and Damage ===" << std::endl;
+    {
+        FragTrap frag("Warrior");
+        std::cout << "\n--- Attacking multiple targets ---" << std::endl;
+        frag.attack("Enemy1");
+        frag.attack("Enemy2");
+        frag.attack("Enemy3");
+        
+        std::cout << "\n--- Taking damage ---" << std::endl;
+        frag.takeDamage(40);
+        frag.takeDamage(30);
+        frag.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 4: FragTrap Repair ===" << std::endl;
+    {
+        FragTrap frag("Healer");
+        frag.takeDamage(70);
+        frag.beRepaired(30);
+        frag.beRepaired(25);
+        frag.attack("Enemy");
+        frag.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 5: Energy Depletion Test ===" << std::endl;
+    {
+        FragTrap frag("Exhausted");
+        std::cout << "\n--- Performing 100 actions to deplete energy ---" << std::endl;
+        for (int i = 0; i < 101; i++)
+        {
+            frag.attack("Target");
+        }
+        std::cout << "\n--- Trying to repair with no energy ---" << std::endl;
+        frag.beRepaired(10);
+        std::cout << "\n--- HighFives (should still work) ---" << std::endl;
+        frag.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 6: Copy Constructor ===" << std::endl;
+    {
+        FragTrap original("Original");
+        original.takeDamage(50);
+        original.attack("Test");
+        
+        std::cout << "\n--- Creating copy ---" << std::endl;
+        FragTrap copy(original);
+        copy.attack("Copy Target");
+        copy.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 7: Assignment Operator ===" << std::endl;
+    {
+        FragTrap frag1("Alpha");
+        FragTrap frag2("Beta");
+        
+        frag1.takeDamage(60);
+        std::cout << "\n--- Assigning frag1 to frag2 ---" << std::endl;
+        frag2 = frag1;
+        frag2.attack("Target");
+        frag2.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 8: Dead FragTrap Cannot Act ===" << std::endl;
+    {
+        FragTrap frag("Deceased");
+        frag.takeDamage(100);
+        frag.attack("Ghost");
+        frag.beRepaired(50);
+        frag.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 9: Mixed ClapTrap, ScavTrap and FragTrap ===" << std::endl;
+    {
+        std::cout << "\n--- Creating ClapTrap ---" << std::endl;
+        ClapTrap clap("BasicBot");
+        
+        std::cout << "\n--- Creating ScavTrap ---" << std::endl;
+        ScavTrap scav("GuardBot");
+        
+        std::cout << "\n--- Creating FragTrap ---" << std::endl;
+        FragTrap frag("DemolitionBot");
+        
+        std::cout << "\n--- ClapTrap actions ---" << std::endl;
+        clap.attack("Target");
+        
+        std::cout << "\n--- ScavTrap actions ---" << std::endl;
+        scav.attack("Target");
+        scav.guardGate();
+        
+        std::cout << "\n--- FragTrap actions ---" << std::endl;
+        frag.attack("Target");
+        frag.highFivesGuys();
+        
+        std::cout << "\n--- Destruction order ---" << std::endl;
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 10: High Damage FragTrap ===" << std::endl;
+    {
+        FragTrap frag("PowerHouse");
+        std::cout << "\n--- FragTrap deals 30 damage per attack ---" << std::endl;
+        for (int i = 0; i < 5; i++)
+        {
+            frag.attack("Monster");
+        }
+        frag.highFivesGuys();
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 11: FragTrap vs ScavTrap Comparison ===" << std::endl;
+    {
+        std::cout << "\n--- Creating both types ---" << std::endl;
+        ScavTrap scav("Guardian");
+        FragTrap frag("Destroyer");
+        
+        std::cout << "\n--- ScavTrap special ability ---" << std::endl;
+        scav.guardGate();
+        
+        std::cout << "\n--- FragTrap special ability ---" << std::endl;
+        frag.highFivesGuys();
+        
+        std::cout << "\n--- Both attacking ---" << std::endl;
+        scav.attack("Enemy");  // 20 damage
+        frag.attack("Enemy");  // 30 damage
+    }
+    std::cout << std::endl;
+
+    std::cout << "=== Test 12: Stress Test - Multiple High Fives ===" << std::endl;
+    {
+        FragTrap frag("Enthusiastic");
+        for (int i = 0; i < 3; i++)
+        {
+            frag.highFivesGuys();
+            frag.attack("Target");
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
