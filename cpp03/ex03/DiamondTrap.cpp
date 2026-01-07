8b7f996dc4589d2cc504540b9770f993df495c4e
HalilAlb <halil_3425@hotmail.com>
2026-01-07 09:15:06 +0300

    diamondtrap'te yorum satırları silindi

--- cpp03/ex03/DiamondTrap.cpp
+++ cpp03/ex03/DiamondTrap.cpp
@@ -14,18 +14,18 @@
 
 DiamondTrap::DiamondTrap() : ClapTrap("Default_clap_name"), ScavTrap(), FragTrap(), _name("Default")
 {
-    _hitPoints = FragTrap::_hitPoints;      // 100 from FragTrap
-    _energyPoints = ScavTrap::_energyPoints;  // 50 from ScavTrap
-    _attackDamage = FragTrap::_attackDamage;  // 30 from FragTrap
+    _hitPoints = FragTrap::_hitPoints;
+    _energyPoints = ScavTrap::_energyPoints;
+    _attackDamage = FragTrap::_attackDamage;
     std::cout << "DiamondTrap default constructor called!" << std::endl;
 }
 
 DiamondTrap::DiamondTrap(const std::string& name) 
     : ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), _name(name)
 {
-    _hitPoints = FragTrap::_hitPoints;      // 100 from FragTrap
-    _energyPoints = ScavTrap::_energyPoints;  // 50 from ScavTrap
-    _attackDamage = FragTrap::_attackDamage;  // 30 from FragTrap
+    _hitPoints = FragTrap::_hitPoints;
+    _energyPoints = ScavTrap::_energyPoints;
+    _attackDamage = FragTrap::_attackDamage;
     std::cout << "DiamondTrap " << _name << " has been constructed!" << std::endl;
 }
 
